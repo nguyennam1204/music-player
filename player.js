a1b27c1fddee969d9e8126e27527fa605d5236a9
nguyennam1204 <nguyenhoainam61021@gmail.com>
2024-03-31 15:29:29 +0700

    Initial commit

---
+++ player.js
@@ -0,0 +1,12 @@
+// const { Amplitude } = require("amplitudejs");
+
+Amplitude.init({
+    songs:[{
+        name:'EquiliBrium I (Cello version)',
+        artist:'David Hilowitz',
+        album:'EquiliBrium I (Cello version)',
+        url:'./music/Equilibrium_I_Cello_version_David_Hilowitz.mp3',
+        cover_art_url:'./music/hing.png',
+     
+    }]
+})
